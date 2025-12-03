f76956017397b84cc628c74804d73636f8c80d73
Andreu <andreu.riba@gracia.lasalle.cat>
2025-12-04 00:33:51 +0100

    leds con temperatura y gota

--- main.py
+++ main.py
@@ -1,3 +1,40 @@
+mode = -1  
+x = 2
+y = 2
+
+def on_button_pressed_a():
+    global mode
+    mode = 0
+
+def on_button_pressed_b():
+    global mode
+    mode = 1
+
+input.on_button_pressed(Button.A, on_button_pressed_a)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
 def on_forever():
-    led.plot_bar_graph(input.temperature(), 50)
+    global x, y, mode
+
+    if mode == 0:
+        led.plot_bar_graph(input.temperature(), 50)
+
+    elif mode == 1:
+        led.plot(x, y)
+        basic.pause(50)
+        led.unplot(x, y)
+
+        accX = input.acceleration(Dimension.X)
+        accY = input.acceleration(Dimension.Y)
+
+        if accX <= 150 and x > 0:
+            x -= 1
+        if accX > 150 and x < 4:
+            x += 1
+
+        if accY <= 150 and y > 0:
+            y -= 1
+        if accY > 150 and y < 4:
+            y += 1
+
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,42 @@
+let mode = -1
+let x = 2
+let y = 2
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    mode = 0
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    mode = 1
+})
 basic.forever(function on_forever() {
-    led.plotBarGraph(input.temperature(), 50)
+    let accX: number;
+    let accY: number;
+    
+    if (mode == 0) {
+        led.plotBarGraph(input.temperature(), 50)
+    } else if (mode == 1) {
+        led.plot(x, y)
+        basic.pause(50)
+        led.unplot(x, y)
+        accX = input.acceleration(Dimension.X)
+        accY = input.acceleration(Dimension.Y)
+        if (accX <= 150 && x > 0) {
+            x -= 1
+        }
+        
+        if (accX > 150 && x < 4) {
+            x += 1
+        }
+        
+        if (accY <= 150 && y > 0) {
+            y -= 1
+        }
+        
+        if (accY > 150 && y < 4) {
+            y += 1
+        }
+        
+    }
+    
 })
